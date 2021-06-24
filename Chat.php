b0ca6882e2f23408227e575876b0e13c1f95980b
Катя Заплатина <49903775+itsnotkatya@users.noreply.github.com>
2021-06-24 10:11:13 +1000

    added html and styles

--- Chat.php
+++ Chat.php
@@ -1,8 +1,67 @@
-<?php
 
-function isAuthorised($login, $password)         //авторизация
+<html>
+<style>
+    html, body {
+        height: 100%;
+        width: 100%;
+    }
+    .chat {
+        border:1px solid #333;
+        margin:15px;
+        width:40%;
+        height:70%;
+        background:#555;
+        color:#fff;
+    }
+    .chat-messages {
+        min-height:93%;
+        max-height:93%;
+        overflow:auto;
+    }
+    .chat-messages__content {
+        padding:1px;
+    }
+
+    .chat-input {
+        min-height:6%;
+    }
+    input {
+        font-family:arial;
+        font-size:16px;
+        vertical-align:middle;
+        background:#333;
+        color:#fff;
+        border:0;
+        display:inline-block;
+        margin:1px;
+        height:30px;
+    }
+    .chat-form__input {
+        width:79%;
+    }
+    .chat-form__submit {
+        width:18%;
+    }
+</style>
+
+<div class='chat'>
+	<div class='chat-messages'>
+		<div class='chat-messages__content' id='messages'>
+    Загрузка...
+		</div>
+	</div>
+	<div class='chat-input'>
+		<form method='post' id='chat-form'>
+			<input type='text' id='message-text' class='chat-form__input' placeholder='Введите сообщение'> <input type='submit' class='chat-form__submit' value='=>'>
+		</form>
+	</div>
+</div>
+</html>
+
+<?php
+function isAuthorised($login, $password)                    //авторизация
 {
-    if (isset($_GET["login"]) &&  isset($_GET["pass"])) {
+    if (isset($_GET["login"]) && isset($_GET["pass"])) {
         if (!isset($users[$_GET["login"]])) {
             echo "User doesn't exist";
         }
@@ -43,6 +102,7 @@ function main() {
         showMessages();                             //если не авторизован, то может только посмотреть чат
     }
 }
+
 
 
 
